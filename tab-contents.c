e3874a9ac53d88a23c7355dc90080bb9db60f7fa
Jon Allen <ylixir@gmail.com>
2010-09-29 01:56:37 -0500

    set up some sane defaults for the editor's behavior. Fixes Issue 14.

--- tab-contents.c
+++ tab-contents.c
@@ -53,6 +53,9 @@ YaedTabContentsHandle yaedTabContentsNew( const YaedSourceModelHandle model,
     //misc variables we need for packing
     GtkScrolledWindow* scroll_area;
     GtkWidget* location_widget;
+    
+    //misc variables for the default setup
+    PangoFontDescription* theFont;
 
     //allocate and fill out the contents
     contents = g_slice_new(struct YaedTabContents);
@@ -65,7 +68,40 @@ YaedTabContentsHandle yaedTabContentsNew( const YaedSourceModelHandle model,
     gtk_container_add((GtkContainer*)scroll_area, (GtkWidget*)contents->text);
     location_widget = yaedLocationBarWidget(contents->location_bar);
     gtk_box_pack_start((GtkBox*)contents->box, location_widget, FALSE, TRUE, 0);
-    gtk_box_pack_start((GtkBox*)contents->box, (GtkWidget*)scroll_area, TRUE, TRUE, 0);
+    gtk_box_pack_start( (GtkBox*)contents->box,
+                        (GtkWidget*)scroll_area,
+                        TRUE,
+                        TRUE,
+                        0);
+
+    /*
+     * set up some defaults
+     */
+
+    //monospace font
+    theFont = pango_font_description_from_string("monospace 12");
+    gtk_widget_modify_font((GtkWidget*)contents->text, theFont);
+    pango_font_description_free(theFont);
+    //default line spacing feels crowded
+    gtk_text_view_set_pixels_above_lines((GtkTextView*)contents->text, 1);
+    gtk_text_view_set_pixels_below_lines((GtkTextView*)contents->text, 1);
+    gtk_text_view_set_pixels_inside_wrap((GtkTextView*)contents->text, 1);
+
+    gtk_source_view_set_insert_spaces_instead_of_tabs(contents->text, TRUE);
+
+    gtk_source_view_set_show_line_numbers(contents->text, TRUE);
+
+    gtk_source_view_set_indent_width(contents->text, 2);
+    gtk_source_view_set_tab_width(contents->text, 2);
+
+    gtk_source_view_set_auto_indent(contents->text, TRUE);
+    
+    gtk_source_view_set_indent_on_tab(contents->text, TRUE);
+
+    gtk_source_view_set_highlight_current_line(contents->text, TRUE);
+
+    gtk_source_view_set_show_right_margin(contents->text, TRUE);
+    gtk_source_view_set_right_margin_position(contents->text, 80);
 
     //it's all ready to go
     gtk_widget_show((GtkWidget*)contents->text);
@@ -85,7 +121,8 @@ bool yaedTabContentsModelUpdate(YaedTabContentsHandle contents,
 
   //get the buffers
   new_buffer = yaedSourceModelGetBuffer(model);
-  old_buffer = (GtkSourceBuffer*)gtk_text_view_get_buffer((GtkTextView*)contents->text);
+  old_buffer =
+      (GtkSourceBuffer*)gtk_text_view_get_buffer((GtkTextView*)contents->text);
 
   //set the buffer
   if(old_buffer != new_buffer)
